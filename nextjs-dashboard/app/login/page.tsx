7d5f08339bff3298f2d5ba0bbd75beaef35d7cfd
Michael Shuster <m.shuster241@gmail.com>
2024-04-20 12:02:48 +1200

    feat: updated logo

--- nextjs-dashboard/app/login/page.tsx
+++ nextjs-dashboard/app/login/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import MDWLogo from '@/app/ui/mdw-logo';
 import LoginForm from '@/app/ui/login-form';
 import Image from 'next/image';
 
@@ -21,8 +21,8 @@ export default function LoginPage() {
       />
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-40% to-emerald-500 to-90% p-3  md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+          <div className="w-auto text-white">
+            <MDWLogo />
           </div>
         </div>
         <LoginForm />
